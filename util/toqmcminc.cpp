8a3d58b0d3f4abb630911db0a9ba9a27b2f0f3b9
yukarinoki <blkheartp@gmail.com>
2021-02-16 16:12:55 +0900

    a

--- util/toqmcminc.cpp
+++ util/toqmcminc.cpp
@@ -9,8 +9,8 @@ int main(){
     int s,t; cin >> s >> t;
     int n,m; cin >> n >> m;
     cout << n << " " << m + 2 << endl;
-    cout << s << " " << s << " " << 100 << endl;
-    cout << t << " " << t << " " << -100 << endl;
+    cout << s << " " << s << " " << 1000 << endl;
+    cout << t << " " << t << " " << -1000 << endl;
     for(int i=0;i<m;i++){
         int a,b,c;
         cin >> a >> b >> c;
