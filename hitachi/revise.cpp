b592698caec52c97c0168f905deec0f2771034bd
yukarinoki <blkheartp@gmail.com>
2021-02-17 09:20:24 +0900

    aa

---
+++ hitachi/revise.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int v,e;
+    cin >> v >> e;
+    cout << v << " " << e << endl;
+    for(int i = 0; i<e; i++){
+        int a;
+        string ss;
+        cin >> a >> ss;
+        if(ss[ss.length()-1] == '-'){
+            cout << a << " " << ss.replace(ss.length() - 2, 1, " -") << endl;
+        }else{
+            cout << a << " " << ss.replace(ss.length() - 1, 1, " " + ss.substr(ss.length()-1,1)) << endl;
+        }
+    }
+
+    return 0;
+}
